31146182aabfc744bb72dc3194a62d4e0ac9ee91
Nitzan Weidenfeld <4964249+vaiden@users.noreply.github.com>
2020-02-06 10:09:34 +0200

    FIXED:

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
 const { EOL } = require('os');
 
 module.exports = function() {
-    let text = decodeURI(Array.prototype.join.call(arguments,' ')).replace(/^'(.+)'$/,''); // We need to remove the '' on Windows
+    let text = decodeURI(Array.prototype.join.call(arguments,' ')).replace(/^'(.+)'$/,'$1'); // We need to remove the '' on Windows
 
     try {
         text = JSON.stringify(JSON.parse(text), null,2);
+        if (process.platform === "win32"){
+            text = text.replace(/\n/g,EOL); // JSON.stringify() doesn't care for CRLFs apparently
+        }
+    }
     } catch (e) {
 
     }
 
     text = text.replace(/\\"/g,'"').replace(/\\\\n/g,EOL).replace(/\\\\t/g,'\t').replace(/\\n/g,EOL).replace(/\\t/g,'\t');
+    
+    
 
     return text;
 }
